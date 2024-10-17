5559f72709a617d81731c9c9f3b1a8e48d259b32
Maksym Usyk <max.taina32@gmail.com>
2024-10-18 02:10:09 +0300

    password in auth service was encrypted

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../../user/service/user.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,10 +10,26 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
+  async hashPassword(password: string): Promise<string> {
+    const salt: string = await bcrypt.genSalt();
+    return bcrypt.hash(password, salt);
+  }
+
+  async comparePasswords(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+
   async signIn(username: string, password: string): Promise<any> {
     const user = await this.usersService.findByUsername(username);
 
-    if (user?.password !== password) {
+    const isAuthenticated: boolean = await this.comparePasswords(
+      password,
+      user?.password,
+    );
+    if (isAuthenticated) {
       throw new UnauthorizedException();
     }
 
@@ -24,6 +41,7 @@ export class AuthService {
   }
 
   async signUp(username: string, password: string): Promise<any> {
-    await this.usersService.create(username, password);
+    const hashedPassword: string = await this.hashPassword(password);
+    await this.usersService.create(username, hashedPassword);
   }
 }
