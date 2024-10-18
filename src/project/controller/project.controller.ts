1f47dc5f1bb27ff8572cef3c0b1bc989d8db052f
Maksym Usyk <max.taina32@gmail.com>
2024-10-18 03:43:39 +0300

    limit, offset and search query params was added to project.controller.ts

--- src/project/controller/project.controller.ts
+++ src/project/controller/project.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards, Query } from '@nestjs/common';
 import { ProjectService } from '../service/project.service';
 import { AuthGuard } from '../../auth/guard/auth.guard';
 import { ProjectListResponse } from '../dto/project-list-response.dto';
@@ -10,11 +10,27 @@ export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   @Get()
-  async list(@Request() req): Promise<ProjectListResponse> {
+  async list(@Request() req, @Query() query): Promise<ProjectListResponse> {
     const userId = req.user.sub as number;
+    const limit = (query.limit as number) ?? 10;
+    const offset = (query.offset as number) ?? 0;
+    const search = query.search as string;
 
     const list: Project[] = await this.projectService.findMany({
-      where: { userId },
+      where: {
+        userId,
+        status: { not: 'deleted' },
+        ...(search
+          ? {
+              OR: [
+                { name: { contains: search } },
+                { url: { contains: search } },
+              ],
+            }
+          : {}),
+      },
+      take: limit,
+      skip: offset,
     });
 
     return list.map((x: Project) => ({
